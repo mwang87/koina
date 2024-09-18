f19da398ffe9cf7a5665f88bce1dca1e592df619
Mingxun Wang <mwang87@gmail.com>
2024-09-17 22:15:05 -0700

    bug fix to have different data

--- models/3dmolms/3dmolms_postprocess/1/model.py
+++ models/3dmolms/3dmolms_postprocess/1/model.py
@@ -20,6 +20,7 @@ class TritonPythonModel:
     def execute(self, requests):
         responses = []
         for request in requests:
+            # qToF
             raw = pb_utils.get_input_tensor_by_name(request, "3dmolms_out")
             norm = raw.as_numpy()
             norm = norm[0]
@@ -34,7 +35,23 @@ class TritonPythonModel:
             # convert to a -1 x 2 numpy array
             norm = np.array(result, dtype=np.float32)
             qtof_ce_tensor = pb_utils.Tensor("qtof_out", norm.astype(self.qtofoutput_dtype))
-            orbi_ce_tensor = pb_utils.Tensor("orbi_out", norm.astype(self.orbioutput_dtype))
+
+            # Orbitrap
+            raw = pb_utils.get_input_tensor_by_name(request, "3dmolms_orbi_out")
+            norm = raw.as_numpy()
+            norm = norm[0]
+
+            # comvert it to mz and intensity pairs
+            result = []
+            bucket_size = 0.2
+            threshold = 0.1
+            for i in range(len(norm)):
+                if norm[i] > threshold:
+                    result.append([i * bucket_size, norm[i]])
+            # convert to a -1 x 2 numpy array
+            norm = np.array(result, dtype=np.float32)
+            qtof_ce_tensor = pb_utils.Tensor("orbi_out", norm.astype(self.qtofoutput_dtype))
+
             responses.append(pb_utils.InferenceResponse(output_tensors=[qtof_ce_tensor, orbi_ce_tensor]))
 
         return responses
